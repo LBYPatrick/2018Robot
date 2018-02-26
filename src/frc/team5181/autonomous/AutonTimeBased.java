e233860d1e809160e567a36fc2ee6c4bb5bec351
tylerliu <tylersiqi@163.com>
2018-02-26 00:36:20 -0500

    Tasking?

--- src/frc/team5181/autonomous/AutonTimeBased.java
+++ src/frc/team5181/autonomous/AutonTimeBased.java
@@ -1,68 +1,75 @@
 package frc.team5181.autonomous;
 
-import edu.wpi.first.wpilibj.DriverStation;
+import frc.team5181.tasking.SyncTask;
+import frc.team5181.tasking.Task;
+import frc.team5181.tasking.TaskSequence;
 
-public class AutonTimeBased implements Runnable {
+public class AutonTimeBased implements Task {
 
     final public static int Left = 0, Middle = 1, Right = 2;
+    private TaskSequence seq;
 
     private int position;
 
     public AutonTimeBased(int position) {
         this.position = position;
+        boolean isLeftSide = AutonHelper.isCorrectSwitchLeft();
+        seq = new TaskSequence().add(
+                new AutonHelper.TimedDrive(0,1,500),
+                new AutonHelper.TimedDrive());
+
+        switch (this.position) {
+            case 0: //Left
+                seq.add(
+                new SyncTask(() -> {AutonHelper.report("LEFT SIDE");}),
+                new AutonHelper.TimedDrive(0, 1, 500), //Go forward for passing the auto-line
+                new AutonHelper.TimedDrive());
+                if(isLeftSide) { //Shoot the cube in if it is the correct side
+                    seq.add(
+                        new SyncTask(() -> {AutonHelper.report("CORRECT SWITCH");}),
+                        new AutonHelper.TimedDrive(1, 0, 180), //Turn Right
+                        new AutonHelper.TimedDrive(),
+                        new AutonHelper.TimedDrive(0, 1, 300), //Go forward
+                        new AutonHelper.TimedDrive()
+                    );
+                }
+                break;
+            case 1: // Middle
+                seq.add(
+                        new SyncTask(() -> {AutonHelper.report("MIDDLE");}),
+                    new AutonHelper.TimedDrive(isLeftSide? -1 : 1, 0, 180),//Turn Left/Right
+                    new AutonHelper.TimedDrive(),
+                    new AutonHelper.TimedDrive(0,1, 300), //Go Forward
+                    new AutonHelper.TimedDrive(),
+                    new AutonHelper.TimedDrive(isLeftSide? 1 : -1, 0,180), //Turn Right/Left
+                    new AutonHelper.TimedDrive(),
+                    new AutonHelper.TimedDrive(0,1,300) //Final Forward
+                );
+                break;
+            case 2: //Right
+                seq.add(
+                        new SyncTask(() -> {AutonHelper.report("RIGHT SIDE");}),
+                        new AutonHelper.TimedDrive(0, 1, 500), //Go forward for passing the auto-line
+                        new AutonHelper.TimedDrive());
+                seq.add(
+                        new SyncTask(() -> {AutonHelper.report("CORRECT SWITCH");}),
+                        new AutonHelper.TimedDrive(-1, 0, 180), //Turn Right
+                        new AutonHelper.TimedDrive(),
+                        new AutonHelper.TimedDrive(0, 1, 300), //Go forward
+                        new AutonHelper.TimedDrive()
+                );
+                break;
+            default:
+                AutonHelper.report("Go Forward only");
+        }
+
+
+        seq.add(new AutonHelper.shootCube(1)); //Shoot the cube out
+        AutonHelper.report("All done.");
+        
     }
 
-    public void run() {
-        try {
-            AutonHelper.tankDrive(0,1,500);
-            AutonHelper.takeABreak();
-            boolean isLeftSide = AutonHelper.isCorrectSwitchLeft();
-
-
-            switch (this.position) {
-                case 0: //Left
-                    AutonHelper.report("LEFT SIDE");
-                    AutonHelper.tankDrive(0, 1, 500); //Go forward for passing the auto-line
-                    AutonHelper.takeABreak();
-                    if(isLeftSide) { //Shoot the cube in if it is the correct side
-                        AutonHelper.report("CORRECT SWITCH");
-                        AutonHelper.tankDrive(1, 0, 180); //Turn Right
-                        AutonHelper.takeABreak();
-                        AutonHelper.tankDrive(0, 1, 300); //Go forward
-                        AutonHelper.takeABreak();
-                    }
-                    break;
-                case 1: // Middle
-                        AutonHelper.report("MIDDLE");
-                        AutonHelper.tankDrive(isLeftSide? -1 : 1, 0, 180); //Turn Left/Right
-                        AutonHelper.takeABreak();
-                        AutonHelper.tankDrive(0,1, 300); //Go Forward
-                        AutonHelper.takeABreak();
-                        AutonHelper.tankDrive(isLeftSide? 1 : -1, 0,180); //Turn Right/Left
-                        AutonHelper.takeABreak();
-                        AutonHelper.tankDrive(0,1,300); //Final Forward
-                    break;
-                case 2: //Right
-                    AutonHelper.report("RIGHT SIDE");
-                    AutonHelper.tankDrive(0,1,500); //Go forward for passing the auto-line
-                    AutonHelper.takeABreak();
-
-                    if(!isLeftSide) {
-                        AutonHelper.report("CORRECT SWITCH");
-                        AutonHelper.tankDrive(-1, 0, 180); //Turn Left
-                        AutonHelper.takeABreak();
-                        AutonHelper.tankDrive(0, 1, 300); //Go forward
-                        AutonHelper.takeABreak();
-                    }
-                    break;
-                default:
-                    AutonHelper.report("Go Forward only");
-            }
-
-            AutonHelper.shootCube(1); //Shoot the cube out
-            AutonHelper.report("All done.");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public boolean nextStep() {
+        return seq.nextStep();
     }
 }
